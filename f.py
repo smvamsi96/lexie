b9eed79779abbb97c932c45fe280dd075e926403
smvamsi96 <smvamsi96@gmail.com>
2020-03-20 19:48:57 +0530

    removed some files

--- f.py
+++
@@ -1,9 +0,0 @@
-import filetype
-
-def main():
-    kind = filetype.guess('/Users/smvamsi/Downloads/nginx.html')
-    print('File extension: %s' % kind.extension)
-    print('File MIME type: %s' % kind.mime)
-
-if __name__ == '__main__':
-    main()
